728b19033777dd0670e306f8b6dca84f82d81179
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-22 17:17:01 +0200

    Update 5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,1 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """Sums up a list of floats."""
+    return sum(input_list)

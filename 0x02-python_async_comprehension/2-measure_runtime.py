302a82198c231a1f1f76bf684cfdeec7516a0032
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-08 23:50:32 +0200

    Update 2-measure_runtime.py

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,20 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Run time for four parallel comprehensions
+"""
+import asyncio
+from time import time
+from typing import List
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Coroutine that measures the total runtime for executing async_comprehension
+    four times in parallel
+    """
+    start_time = time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end_time = time()
+    return end_time - start_time

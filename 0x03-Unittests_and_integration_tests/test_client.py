0d89607f3553cb8d66847994f11db0e8d5450351
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-11 00:49:09 +0200

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -17,7 +17,9 @@ class TestGithubOrgClient(unittest.TestCase):
     ])
     @patch('client.get_json')
     def test_org(self, org_name, mock_get_json):
-        """Test GithubOrgClient.org returns correct JSON response"""
+        """Test that GithubOrgClient.org returns
+        the correct JSON response.
+        """
         url = f"https://api.github.com/orgs/{org_name}"
         expected_org = {"login": org_name, 'url': url}
         mock_get_json.return_value = expected_org
@@ -28,7 +30,9 @@ class TestGithubOrgClient(unittest.TestCase):
 
     @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
     def test_public_repos_url(self, mock_org):
-        """Test GithubOrgClient._public_repos_url returns correct response"""
+        """Test that GithubOrgClient._public_repos_url
+        returns the correct JSON response.
+        """
         url = "https://api.github.com/orgs/google/repos"
         mock_org.return_value = {"repos_url": url}
 
@@ -40,7 +44,9 @@ class TestGithubOrgClient(unittest.TestCase):
 
     @patch('client.get_json')
     def test_public_repos(self, mock_get_json):
-        """Test GithubOrgClient.public_repos returns correct response"""
+        """Test that GithubOrgClient.public_repos
+        returns the correct JSON response.
+        """
         mock_repo_data = [
                 {"name": "repo1", "license": {"key": "mit"}},
                 {"name": "repo2", "license": {"key": "apache-2.0"}},
@@ -63,7 +69,9 @@ class TestGithubOrgClient(unittest.TestCase):
         ({"license": {"key": "other_license"}}, "my_license", False),
     ])
     def test_has_license(self, r, l_key, exp):
-        """Test GithubOrgClient.has_license returns correct response"""
+        """Test that GithubOrgClient.has_license
+        returns the correct JSON response.
+        """
         self.assertEqual(GithubOrgClient.has_license(r, l_key), exp)
 
 
@@ -72,16 +80,17 @@ class TestGithubOrgClient(unittest.TestCase):
         (org_payload, repos_payload, expected_repos, apache2_repos),
 ])
 class TestIntegrationGithubOrgClient(unittest.TestCase):
-    """Integration tests for GithubOrgClient"""
+    """Integration test
+    """
 
     @classmethod
     def setUpClass(cls):
-        """Set up class for integration tests"""
+        """setUpClass"""
         cls.get_patcher = patch('client.requests.get')
         cls.mock_get = cls.get_patcher.start()
 
         def side_effect(url, *args, **kwargs):
-            """Side effect for mocking requests.get"""
+            """side_effect"""
             if 'orgs' in url:
                 return Mock(json=lambda: cls.org_payload)
             elif 'repos' in url:
@@ -93,7 +102,7 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        """Tear down class for integration tests"""
+        """tearDownClass"""
         cls.get_patcher.stop()
 
     @parameterized.expand([
@@ -101,7 +110,9 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         ("abc",),
     ])
     def test_public_repos(self):
-        """Test GithubOrgClient.public_repos returns correct response in integration"""
+        """Test that GithubOrgClient.org returns
+        the correct JSON response.
+        """
         with patch('client.get_json') as mocked_get_json:
             mocked_get_json.side_effect = [
                 org_payload,
@@ -113,7 +124,9 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
             mocked_get_json.assert_called()
 
     def test_public_repos_with_license(self):
-        """Test GithubOrgClient.public_repos returns correct response with license in integration"""
+        """Test that GithubOrgClient.public_repos
+        returns the correct JSON response.
+        """
         with patch('client.get_json') as mocked_get_json:
             mocked_get_json.side_effect = [
                 org_payload,

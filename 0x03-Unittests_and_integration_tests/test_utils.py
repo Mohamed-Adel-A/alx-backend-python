52649829d90f3392ec7e5fa6608ae0e0ed7f3a3e
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-11 00:21:39 +0200

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,1 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Test cases for utils module
+"""
+import unittest
+from utils import access_nested_map
+
+
+class TestUtils(unittest.TestCase):
+    """Test cases for utils module"""
+
+    def test_access_nested_map(self):
+        """Test access_nested_map"""
+        nested_map = {"a": {"b": {"c": 1}}}
+        self.assertEqual(access_nested_map(nested_map, ["a", "b", "c"]), 1)
